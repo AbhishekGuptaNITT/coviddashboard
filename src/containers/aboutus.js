7c5627053a62bc2fef9885cb58bf27cf4709b1d9
Abhishek <205119002@nitt.edu>
2021-02-05 22:16:28 +0530

    changed about page

--- src/containers/aboutus.js
+++ src/containers/aboutus.js
@@ -3,7 +3,7 @@ import { Jumbotron,Row } from 'react-bootstrap'
 const Aboutus = (props) => {
     return(
         
-        <Jumbotron style={{margin:'50px',background:'black',color:'skyblue'}}>
+        <Jumbotron style={{margin:'50px 25px',background:'black',color:'skyblue'}}>
             <h1 className='display-4'>Covid dashboard</h1>
             <p className='h4 text-success'>
                 <u><i>Created with care by an NIT-Trichyian</i></u>
